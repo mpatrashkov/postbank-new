74caec73dc0118c980196b99c90f49ac02a438ea
Georgi Atanasov <36049559+nigosto@users.noreply.github.com>
2019-06-16 14:57:56 +0300

    ассаса

--- components/Summary.js
+++ components/Summary.js
@@ -3,6 +3,15 @@ import { View, Text } from "react-native";
 import Colors from '../Colors';
 
 class Summary extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            steps: undefined
+        }
+    }
+
     render() {
         return (
             <View style={{height: 100, backgroundColor: Colors.primary, flexDirection: "row"}}>
@@ -14,6 +23,24 @@ class Summary extends Component {
                 </View>
             </View>
         );
+    }
+
+    async componentDidMount() {
+        let data = await fetch("http://192.168.43.27:9999/activity/steps", {
+            method: "POST",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                token: result
+            })
+
+        });
+        let res = await data.json(); 
+        this.setState({
+            steps: res.steps
+        })
     }
 }
 
--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -50,33 +50,61 @@ class SignInScreen extends Component {
         try {
             const result = await Google.logInAsync({
                 androidClientId: "977535412978-8u4m0at7rjldvsllhsojggjep4srl2bb.apps.googleusercontent.com",
-                scopes: [ "profile", "email" ]
+                scopes: ["profile", "email", "https://www.googleapis.com/auth/fitness.activity.read",
+                        "https://www.googleapis.com/auth/fitness.activity.write",
+                        "https://www.googleapis.com/auth/fitness.blood_glucose.read",
+                        "https://www.googleapis.com/auth/fitness.blood_glucose.write",
+                        "https://www.googleapis.com/auth/fitness.blood_pressure.read",
+                        "https://www.googleapis.com/auth/fitness.blood_pressure.write",
+                        "https://www.googleapis.com/auth/fitness.body.read",
+                        "https://www.googleapis.com/auth/fitness.body.write",
+                        "https://www.googleapis.com/auth/fitness.body_temperature.read",
+                        "https://www.googleapis.com/auth/fitness.body_temperature.write",
+                        "https://www.googleapis.com/auth/fitness.location.read",
+                        "https://www.googleapis.com/auth/fitness.location.write",
+                        "https://www.googleapis.com/auth/fitness.nutrition.read",
+                        "https://www.googleapis.com/auth/fitness.nutrition.write",
+                        "https://www.googleapis.com/auth/fitness.oxygen_saturation.read",
+                        "https://www.googleapis.com/auth/fitness.oxygen_saturation.write",
+                        "https://www.googleapis.com/auth/fitness.reproductive_health.read",
+                        "https://www.googleapis.com/auth/fitness.reproductive_health.write"]
             });
-    
-            if(result.type === "success") {
+
+            if (result.type === "success") {
                 return result.accessToken;
             }
             else {
                 return { cancelled: true }
             }
         }
-        catch(e) {
+        catch (e) {
             return { error: true }
         }
     }
 
     onClick = async () => {
         const result = await this.signIn();
-        if(result.cancelled) {
-            
+        if (result.cancelled) {
+
         }
-        else if(result.error) {
-            
+        else if (result.error) {
+
         }
         else {
-            fetchPost("activity/steps", {
-                token: result
+            let data = await fetch("http://192.168.43.27:9999/activity/steps", {
+                method: "POST",
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    token: result
+                })
+
             });
+            let res = await data.json();
+            //console.log(res)
+            //localStorage.setItem("token", result);
             this.props.navigation.replace("Main");
         }
     }
